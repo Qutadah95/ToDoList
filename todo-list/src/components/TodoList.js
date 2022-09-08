3bcb5b35be9544588d2cb74c54aa9045bd01866b
Qutadah Almomani <qutadaalmomani@yahoo.com>
2022-09-08 10:55:46 +0300

    integration done

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -8,11 +8,11 @@ const TodoList = ({ todos }) => (
   
   <ul className="todo-row">
 
-    {/* {todos && todos.length */}
-  {/* //     ? todos.map((todo, index) => { */}
-  {/* //       return <Todo key={`todo-${todo.id}`} todo={todo} />; */}
-  {/* //     }) */}
-  {/* //     : "No todos, yay!"} */}
+{todos && todos.length
+   ? todos.map((todo, index) => { 
+     return <Todo key={`todo-${todo.id}`} todo={todo} />; 
+   }) 
+    : "No todos, yay!"}
   </ul>
 );
 
