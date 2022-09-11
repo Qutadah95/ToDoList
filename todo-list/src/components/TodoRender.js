fcf78d3f5c98b4a15c963b3baefb5a06dc47dfbd
Qutadah Almomani <qutadaalmomani@yahoo.com>
2022-09-11 14:34:48 +0300

    update todo

--- todo-list/src/components/TodoRender.js
+++ todo-list/src/components/TodoRender.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 import React, {  useState } from 'react'
+import Modal from "react-bootstrap/Modal";
+import { Button, Form } from "react-bootstrap";
 
  function TodoRender() {
   const [todo,setTodo]=useState('')
+  const [updatedTodo, setUpdatedTodo] = useState([{}])
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
     // let item={todo.todo,};
     // useEffect(()=>{
     //   fetch('http://localhost:8080/list').then(res=>{
@@ -16,14 +24,14 @@ import React, {  useState } from 'react'
         .then((response) => {
           const data = response.data;
           setTodo(data)
-          console.log('Data has been received!!');
+          // console.log('Data has been received!!');
         })
         .catch(() => {
           alert('Error retrieving data!!!');
         });
-        console.log("ddddd",todo);
+        // console.log("ddddd",todo);
 
-        const deletePost = (id) => {
+        const deleteTodo = (id) => {
           console.log(id);
       
           axios
@@ -33,10 +41,38 @@ import React, {  useState } from 'react'
       
           window.location.reload();
         };
-    
+        const updateTodo = (id, todo) => {
+          setUpdatedTodo((prev) => {
+            return {
+              ...prev,
+              id: id,
+              todo: todo,
+            };
+          });
+          handleShow();
+        };
+        const handleChange = (e) => {
+          const { name, value } = e.target;
+          setUpdatedTodo((prev) => {
+            return {
+              ...prev,
+              [name]: value,
+            };
+          });
+        };
+        const saveUpdatedTodo = () => {
+      
+          axios
+            .put(`/update/${updatedTodo.id}`, updatedTodo)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+      
+          handleClose();
+          window.location.reload();
+        };
+      
 
     // ruselt=  ruselt.json();
-    // console.warn('result',ruselt);
  
   
   return (
@@ -47,15 +83,45 @@ import React, {  useState } from 'react'
 {todo && todo.length
    ? todo.map((todoo, index) => { 
      return<><li>{todoo.todo}</li> <button
-     onClick={() => deletePost(todoo._id)}
+     onClick={() => deleteTodo(todoo._id)}
     
    >
      DELETE
-   </button></> ; 
+   </button>
+   <button
+                    onClick={() =>
+                      updateTodo(todoo._id, todoo.todo)
+                    }
+                  >
+                    UPDATE
+                  </button>
+   </> ; 
    }) 
     : "No todos, yay!"}
   </ul> 
-
+  <Modal show={show} onHide={handleClose}>
+        <Modal.Header >
+          <Modal.Title>Update a todo</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Control
+            placeholder="todo"
+            name="todo"
+            value={updatedTodo.todo ? updatedTodo.todo : ""}
+            
+            onChange={handleChange}
+          />
+       
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={saveUpdatedTodo}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
 {/* <ul className="todo-row">
 
 {todo && todo.length
